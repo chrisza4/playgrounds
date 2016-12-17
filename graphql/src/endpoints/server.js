da320318b87b58fd0507bb09f44f54b39d198b91
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2016-12-17 14:16:46 +0700

    Create proxy for both front and back

--- graphql/src/endpoints/server.js
+++ graphql/src/endpoints/server.js
@@ -4,14 +4,35 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from './graphQlSchemas'
+import io from 'socket.io'
+import Http from 'http'
+import proxy from 'express-http-proxy'
 
-export default function createExpressServer () {
+export default function createServer () {
+  createExpressServer()
   const app = express()
-  app.use('/graphql', graphqlHTTP({
+
+  // TODO: Proper configuration on server port and address
+  app.use('/api', proxy('http://localhost:4001'))
+  app.use('/', proxy('http://localhost:3000'))
+  app.listen(4000)
+}
+
+function createExpressServer () {
+  const backendApp = express()
+  backendApp.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root,
+    rootValue: { },
     graphiql: true
   }))
-  app.listen(4000)
-  console.log('Running a GraphQL API server at localhost:4000/graphql')
+  const server = Http.Server(backendApp)
+  const socketIO = io(server)
+  // TODO: Proper configuration on server port and address
+  server.listen(4001)
+  console.log('Running a GraphQL API server at localhost:4001/api/graphql')
+
+  socketIO.on('connection', socket => {
+    console.log('USER CONNECTED')
+  })
 }
+
