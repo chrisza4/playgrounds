7385439ac890f922fab2d1ab3862e117104ad2f9
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2016-12-18 13:16:18 +0700

    Room reducer

--- graphql/front-chat/src/Rooms/reducers.js
+++ graphql/front-chat/src/Rooms/reducers.js
@@ -1,7 +1,19 @@
+import Actions from '../store/Actions'
+import _ from 'lodash'
+
 const initialState = {
 
 }
 
 export default function roomReducer (state = initialState, action) {
-
+  switch (action.type) {
+    case Actions.FETCH_STARTER: {
+      const rooms = action.data.rooms
+      const roomsNoMessages = _(rooms).map(d => _.omit(d, 'messages')).value()
+      return _.keyBy(roomsNoMessages, r => r._id)
+    }
+    default: {
+      return state
+    }
+  }
 }
