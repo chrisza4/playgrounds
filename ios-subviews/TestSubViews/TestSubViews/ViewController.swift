809e0a4a9749cf942c384d58afa1da91ff39cba5
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2017-01-05 06:09:37 +0700

    Refactor

--- ios-subviews/TestSubViews/TestSubViews/ViewController.swift
+++ ios-subviews/TestSubViews/TestSubViews/ViewController.swift
@@ -15,23 +15,21 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    if let redView = Bundle.main.loadNibNamed("RedView", owner: self, options: nil)!.first as? RedView {
-      topView.addSubview(redView)
-      redView.translatesAutoresizingMaskIntoConstraints = false
-      topView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view":redView]))
-      topView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view":redView]))
-    }
-    if let blueView = Bundle.main.loadNibNamed("BlueView", owner: self, options: nil)!.first as? BlueView {
-      bottomView.addSubview(blueView)
-      blueView.translatesAutoresizingMaskIntoConstraints = false
-      bottomView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view":blueView]))
-      bottomView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view":blueView]))
+    self.loadViewFromNibIntoSubview(topView, nibNamed: "RedView")
+    self.loadViewFromNibIntoSubview(bottomView, nibNamed: "BlueView")
+  }
+  
+  func loadViewFromNibIntoSubview(_ view: UIView, nibNamed: String) {
+    if let subView = Bundle.main.loadNibNamed(nibNamed, owner: self, options: nil)!.first as? UIView {
+      view.addSubview(subView)
+      subView.translatesAutoresizingMaskIntoConstraints = false
+      view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view":subView]))
+      view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view":subView]))
     }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
 
 }
