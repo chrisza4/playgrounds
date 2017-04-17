bdd7bf9bb795431674dfde0d731e02bf6525bb93
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2017-04-17 22:37:20 +0700

    Confusing

--- clojurescript-react/src/cljs/clojurescript_react/todo/todoapp.cljs
+++ clojurescript-react/src/cljs/clojurescript_react/todo/todoapp.cljs
@@ -16,31 +16,33 @@
     "active" (filter #(not (:completed %)) todos)
     "completed" (filter #(:completed %) todos)))
 
-(def todo-filter (r/atom "all"))
+; (def todo-filter (r/atom "all"))
 
-(defn todo-board [{:keys [items]}]
- (let [filtered-todos (filter-todo (vals items) @todo-filter)]
-  [:div
-   [:div{:class "todoapp"}
-    [todo-header/todo-header {:on-save todo-state/add}]
-    [:ul {:class "todo-list"}
-     (doall
-      (map
-       #(let [{:keys [editing completed title id]} %]
-             (todo-item/todo-item-component {:editing editing
-                                             :completed completed
-                                             :title title
-                                             :on-toggle todo-state/toggle
-                                             :on-delete todo-state/delete
-                                             :on-edit todo-state/toggle-edit
-                                             :on-edit-title todo-state/edit
-                                             :key id}))
-       filtered-todos))]
-    [todo-footer/todo-footer {:count (count (filter-todo filtered-todos "active"))
-                              :filter @todo-filter
-                              :on-filter #(do
-                                            (js/console.log "change to:" %)
-                                            (reset! todo-filter %))}]]]))
+(defn todo-board []
+ (let [todo-filter (r/atom "all")] ; Why this won't work
+  (fn [{:keys [items]}]
+    (let [filtered-todos (filter-todo (vals items) @todo-filter)]
+     [:div
+      [:div{:class "todoapp"}
+       [todo-header/todo-header {:on-save todo-state/add}]
+       [:ul {:class "todo-list"}
+        (doall
+         (map
+          #(let [{:keys [editing completed title id]} %]
+                (todo-item/todo-item-component {:editing editing
+                                                :completed completed
+                                                :title title
+                                                :on-toggle todo-state/toggle
+                                                :on-delete todo-state/delete
+                                                :on-edit todo-state/toggle-edit
+                                                :on-edit-title todo-state/edit
+                                                :key id}))
+          filtered-todos))]
+       [todo-footer/todo-footer {:count (count (filter-todo filtered-todos "active"))
+                                 :filter @todo-filter
+                                 :on-filter #(do
+                                               (js/console.log "change to:" %)
+                                               (reset! todo-filter %))}]]]))))
 
 (defn todo-app []
   [todo-board {:items @todo-state/todo-data}])
