fe79ec988f16be70df88c59955ca19c4fed7983a
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2017-04-17 14:20:42 +0700

    Little bit refactoring

--- clojurescript-react/src/cljs/clojurescript_react/todo/todoapp.cljs
+++ clojurescript-react/src/cljs/clojurescript_react/todo/todoapp.cljs
@@ -16,29 +16,28 @@
     "active" (filter #(not (:completed %)) todos)
     "completed" (filter #(:completed %) todos)))
 
-(defn todo-board []
- (let [todo-filter (r/atom "all")]
-  (fn [{:keys [items]}]
-   (let [filtered-todos (filter-todo (vals items) @todo-filter)]
-    [:div
-     [:div{:class "todoapp"}
-      [todo-header/todo-header {:on-save todo-state/add}]
-      [:ul {:class "todo-list"}
-       (doall
-        (map
-         #(let [{:keys [editing completed title id]} %]
-               (todo-item/todo-item-component {:editing editing
-                                               :completed completed
-                                               :title title
-                                               :on-toggle todo-state/toggle
-                                               :on-delete todo-state/delete
-                                               :on-edit todo-state/toggle-edit
-                                               :on-edit-title todo-state/edit
-                                               :key id}))
-         filtered-todos))]
-      [todo-footer/todo-footer {:count (count (filter-todo filtered-todos "active"))
-                                :filter @todo-filter
-                                :on-filter #(reset! todo-filter %)}]]]))))
+(defn todo-board [{:keys [items]}]
+ (let [todo-filter (r/atom "all")
+       filtered-todos (filter-todo (vals items) @todo-filter)]
+  [:div
+   [:div{:class "todoapp"}
+    [todo-header/todo-header {:on-save todo-state/add}]
+    [:ul {:class "todo-list"}
+     (doall
+      (map
+       #(let [{:keys [editing completed title id]} %]
+             (todo-item/todo-item-component {:editing editing
+                                             :completed completed
+                                             :title title
+                                             :on-toggle todo-state/toggle
+                                             :on-delete todo-state/delete
+                                             :on-edit todo-state/toggle-edit
+                                             :on-edit-title todo-state/edit
+                                             :key id}))
+       filtered-todos))]
+    [todo-footer/todo-footer {:count (count (filter-todo filtered-todos "active"))
+                              :filter @todo-filter
+                              :on-filter #(reset! todo-filter %)}]]]))
 
 (defn todo-app []
   [todo-board {:items @todo-state/todo-data}])
